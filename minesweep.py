e59e1823dde3d75652f761b38067c14c64be2763
Ashlynn <ashlynn.steeves@icloud.com>
2021-01-16 13:36:04 -0800

    indexing fixed

--- minesweep.py
+++ minesweep.py
@@ -12,41 +12,39 @@ pygame.init()
 
 
 # User input game parameters
-
-usr_grid_y = int(input("Grid height: ")) # x-axis == grid height
-usr_grid_x = int(input("Grid width: ")) # y-axis == grid width 
-mines_pct = int(input("Percentage of mines: "))
+'''
+usr_grid_x = int(input("Grid Width: ")) # x-axis == grid width 
+usr_grid_y = int(input("Grid Height: ")) # y-axis == grid height
+mines_pct = int(input("Percentage of Mines: "))
 '''
 
-usr_grid_y = 6
-usr_grid_x = 5
+usr_grid_x = 6
+usr_grid_y = 5
 mines_pct = 20
-'''
-number_of_cells = usr_grid_height*usr_grid_width
+
+number_of_cells = usr_grid_x*usr_grid_y
 mines_num = math.ceil(number_of_cells*(mines_pct/100))
 
 print("Your minesweeper game has {} cells, and {} mines".format(number_of_cells, mines_num))
 
-# colors 
+# Colors 
 black = (0,0,0)
 white = (255,255,255)
 red = (255, 0, 0)
 
-# Initialize grid 
-mines_grid = [[0 for x in range(usr_grid_width)] for y in range(usr_grid_height)]
-flags_grid = [[0 for x in range(usr_grid_width)] for y in range(usr_grid_height)]
+# Initialize grids 
+mines_grid = [[0 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
+flags_grid = [[0 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
 
-# place mines 
-indices = [(m,n) for m in range(usr_grid_width) for n in range(usr_grid_height)]
+# Randomly place mines 
+indices = [(x,y) for x in range(usr_grid_x) for y in range(usr_grid_y)]
 mine_idx = random.sample(indices, mines_num)
 
-print(mine_idx)
-
 for mine in mine_idx:
-	width = mine[0]
-	height = mine[1]
+	x = mine[0]
+	y = mine[1]
 	
-	mines_grid[height][width] = 9
+	mines_grid[x][y] = 9
 
 def get_adj(idx, grid):
 	x = idx[0] 
@@ -62,33 +60,27 @@ def get_adj(idx, grid):
 
 	adj.append(grid[x-1][y+1]) #bottom left
 	adj.append(grid[x][y+1]) #bottom
-
 	adj.append(grid[x+1][y+1]) #bottom right
 
 	counter = 0
 
 	for x in adj:
 		if x == 9:
-			print(x)
 			counter +=1
-
-	print(counter)
 	return(counter)
 
-print(mines_grid)
-print(mines_grid[1])
+
 test = get_adj([2,3], mines_grid)
-print(mines_grid[2][3])
 mines_grid[2][3] = 10
 
 # Initialize window size
-cell_height = 35
-cell_width = 35
+cell_x = 35
+cell_y = 35
 margin = 5
 
-grid_width = (cell_width*usr_grid_width)+(margin*usr_grid_width)+(margin*2)
-grid_height = (cell_height*usr_grid_height)+(margin*usr_grid_height)+(margin*2)
-grid_size = grid_width, grid_height 
+grid_x = (cell_x*usr_grid_x)+(margin*usr_grid_x)+(margin*2)
+grid_y = (cell_y*usr_grid_y)+(margin*usr_grid_y)+(margin*2)
+grid_size = grid_x, grid_y 
 screen = pygame.display.set_mode(grid_size)
 
 
@@ -117,14 +109,14 @@ while not done:
 	screen.fill(black)
  
 	# --- Drawing code should go here
-	for row in range(usr_grid_height):
-		for column in range(usr_grid_width):
+	for y in range(usr_grid_y):
+		for x in range(usr_grid_x):
 				color = white
-				if mines_grid[row][column]==9:
+				if mines_grid[x][y]==9:
 					color = red
-				if mines_grid[row][column]==10:
+				if mines_grid[x][y]==10:
 					color = black
-				pygame.draw.rect(screen, color, [(margin+cell_width)*column+margin, (margin+cell_height)*row+margin, cell_width, cell_height ])
+				pygame.draw.rect(screen, color, [(margin+cell_x)*x+margin, (margin+cell_y)*y+margin, cell_x, cell_y ])
 
 
  
