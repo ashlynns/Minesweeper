eec52fd099ca81082675ac98e70057771ef9db8f
Ashlynn <ashlynn.steeves@icloud.com>
2021-01-26 16:12:23 -0800

    winning, losing and auto-opening

--- minesweep.py
+++ minesweep.py
@@ -19,8 +19,8 @@ usr_grid_y = int(input("Grid Height: ")) # y-axis == grid height
 mines_pct = int(input("Percentage of Mines: "))
 '''
 
-usr_grid_x = 6
-usr_grid_y = 5
+usr_grid_x = 10
+usr_grid_y = 10
 mines_pct = 20
 
 number_of_cells = usr_grid_x*usr_grid_y
@@ -32,21 +32,32 @@ print("Your minesweeper game has {} cells, and {} mines".format(number_of_cells,
 black = (0,0,0)
 white = (255,255,255)
 red = (255, 0, 0)
-grey = (128, 128, 128)
+green = (0, 255, 0)
 
-# Initialize grids 
-mines_grid = [[0 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
-flags_grid = [[12 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
 
-# Randomly place mines 
-indices = [(x,y) for x in range(usr_grid_x) for y in range(usr_grid_y)]
-mine_idx = random.sample(indices, mines_num)
+# Initialize window size
+cell_x = 35
+cell_y = 35
+margin = 5
 
-for mine in mine_idx:
-	x = mine[0]
-	y = mine[1]
-	
-	mines_grid[x][y] = 9
+grid_x = (cell_x*usr_grid_x)+(margin*usr_grid_x)+(margin*2)
+grid_y = (cell_y*usr_grid_y)+(margin*usr_grid_y)+(margin*2)
+grid_size = grid_x, grid_y 
+screen = pygame.display.set_mode(grid_size)
+
+# Text numbers 
+font = pygame.font.Font('freesansbold.ttf', 24)
+zero = font.render('0', True, black, white)
+one = font.render('1', True, black, white)
+two = font.render('2', True, black, white)
+three = font.render('3', True, black, white)
+four = font.render('4', True, black, white)
+five = font.render('5', True, black, white)
+six = font.render('6', True, black, white)
+seven = font.render('7', True, black, white)
+eight = font.render('8', True, black, white)
+flag = font.render('!', True, black, white)
+bomb = font.render('B', True, black, red)
 
 def get_adj(idx, grid, x_len, y_len):
 	x = idx[0] 
@@ -99,14 +110,14 @@ def get_adj(idx, grid, x_len, y_len):
 			adj[4] = grid[x+1][y] #middle right
 	
 	elif x == x_len:
-		adj[0] =grid[x-1][y-1] #top left
+		adj[0] = grid[x-1][y-1] #top left
 		adj[1] = grid[x][y-1] #top
 		adj[3] = grid[x-1][y] #middle left
 		adj[5] = grid[x-1][y+1] #bottom left
 		adj[6] = grid[x][y+1] #bottom
 
 	else: 		
-		adj[0] =grid[x-1][y-1] #top left
+		adj[0] = grid[x-1][y-1] #top left
 		adj[1] = grid[x][y-1] #top
 		adj[2] = grid[x+1][y-1] #top right 
 
@@ -117,55 +128,76 @@ def get_adj(idx, grid, x_len, y_len):
 		adj[6] = grid[x][y+1] #bottom
 		adj[7] = grid[x+1][y+1] #bottom right
 
-	#counter = 0
-	#for x in adj:
-	#	if x == 9:
-	#		counter +=1
 	return(adj)
 
-for y in range(usr_grid_y): # Loop through all indces to get adj values 
-	for x in range(usr_grid_x):
-		if mines_grid[x][y] != 9: # dont overwrite the mines 
-			adj = get_adj([x,y], mines_grid, usr_grid_x, usr_grid_y)
-			counter = 0 
-			for i in adj: 
-				if i == 9: 
-					counter +=1
-			mines_grid[x][y] = counter
-			#mines_grid[x][y] = get_adj([x,y], mines_grid, usr_grid_x, usr_grid_y)
+# Initialize grids 
+mines_grid = [[0 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
+flags_grid = [[12 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
+win_grid = [[0 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
+lose_grid = [[12 for y in range(usr_grid_y)] for x in range(usr_grid_x)]
 
+indices = [(x,y) for x in range(usr_grid_x) for y in range(usr_grid_y)]
 
-# Initialize window size
-cell_x = 35
-cell_y = 35
-margin = 5
+def place_adj(pos_x, pos_y): 
+	indices.remove((pos_x, pos_y)) # makes sure first click isnt a mine 
+	mine_idx = random.sample(indices, mines_num) # chooses mine indexes 
+	for mine in mine_idx: 
+		x = mine[0]
+		y = mine[1]
+		mines_grid[x][y] = 9 # places mines 
+		lose_grid[x][y] = 9
+		win_grid[x][y] = 10 
 
-grid_x = (cell_x*usr_grid_x)+(margin*usr_grid_x)+(margin*2)
-grid_y = (cell_y*usr_grid_y)+(margin*usr_grid_y)+(margin*2)
-grid_size = grid_x, grid_y 
-screen = pygame.display.set_mode(grid_size)
+		for y in range(usr_grid_y): 
+			for x in range(usr_grid_x):
+				if mines_grid[x][y] != 9: 
+					adj = get_adj([x,y], mines_grid, usr_grid_x, usr_grid_y)
+					counter = 0 
+					for i in adj: 
+						if i ==9: 
+							counter+=1
+					mines_grid[x][y] = counter # fills remainder of the grid 	
+					win_grid[x][y] = counter 	
 
-# Text numbers 
-font = pygame.font.Font('freesansbold.ttf', 22)
-zero = font.render('0', True, black, white)
-one = font.render('1', True, black, white)
-two = font.render('2', True, black, white)
-three = font.render('3', True, black, white)
-four = font.render('4', True, black, white)
-five = font.render('5', True, black, white)
-six = font.render('6', True, black, white)
-seven = font.render('7', True, black, white)
-eight = font.render('8', True, black, white)
-flag = font.render('!', True, black, white)
-bomb = font.render('B', True, black, red)
+def adj_idx_map(idx, pos_x, pos_y): 
+	if idx == 0: 
+		return(pos_x-1, pos_y-1)
+	elif idx == 1: 
+		return(pos_x, pos_y-1)
+	elif idx == 2: 
+		return(pos_x+1, pos_y-1)
+	elif idx == 3:
+		return(pos_x-1, pos_y)
+	elif idx == 4: 
+		return(pos_x+1, pos_y)
+	elif idx == 5: 
+		return(pos_x-1, pos_y+1)
+	elif idx == 6: 
+		return(pos_x, pos_y+1)
+	elif idx == 7:  
+		return(pos_x+1, pos_y+1)
 
+def open_adj(pos_x, pos_y):
+	adj = get_adj([pos_x,pos_y], mines_grid, usr_grid_x, usr_grid_y)
+	for idx, ad in enumerate(adj): 
+		if ad != 11 and ad != 9: 
+			idx_x, idx_y = adj_idx_map(idx, pos_x, pos_y)
+			flags_grid[idx_x][idx_y] = ad
+			lose_grid[idx_x][idx_y] = ad
+			if ad == 0: 
+				adj1 = get_adj([idx_x,idx_y], mines_grid, usr_grid_x, usr_grid_y)
+				for idx1, ad1 in enumerate(adj1): 
+					if ad1 != 11 and ad1 != 9: 
+						idx_x1, idx_y1 = adj_idx_map(idx1, idx_x, idx_y)
+						flags_grid[idx_x1][idx_y1] = ad1
+						lose_grid[idx_x1][idx_y1] = ad1
 
 pygame.display.set_caption("Minesweeper")
  
-# Loop until the user clicks the close button.
-#first click --> 
-done = False
-lost = False 
+
+done = False # Loop until user clicks close button 
+lost = False # Loop until game is lost 
+click_count = 0 
 clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
@@ -178,23 +210,36 @@ while not done:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				done = True
+				lost = True
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				pos = pygame.mouse.get_pos() # location of click 
 				pos_x = pos[0]//(cell_x+margin) # Map location to index 
 				pos_y = pos[1]//(cell_y+margin)
+				if click_count == 0: # First click -- generates the locations of the mines 
+					place_adj(pos_x, pos_y) # place adjacent values 
+					open_adj(pos_x, pos_y)
+					click_count +=1
+					#print(mines_grid)
+
 				if event.button == 1: # left click 
-					if mines_grid[pos_x][pos_y] == 9:
+					if mines_grid[pos_x][pos_y] == 9: # clicked on a mine, game over
 						# clicked on a mine and game over 
 						screen.fill(red)
-						flags_grid[pos_x][pos_y] = mines_grid[pos_x][pos_y]
+						flags_grid = lose_grid
 						lost = True
 					else:
 						flags_grid[pos_x][pos_y] = mines_grid[pos_x][pos_y]
+						lose_grid[pos_x][pos_y] = mines_grid[pos_x][pos_y]
 
 				elif event.button == 3: # right click 
 					flags_grid[pos_x][pos_y] = 10
-	 
-		# --- Drawing code should go here
+
+				if flags_grid == win_grid: 
+					screen.fill(green)
+					lost = True
+
+
+		# --- Drawing code
 		for y in range(usr_grid_y):
 			for x in range(usr_grid_x):
 				if flags_grid[x][y] != 9: 
@@ -234,6 +279,7 @@ while not done:
 				else: 
 					text_rect = bomb.get_rect(center = ((margin+cell_x)*x+margin+(0.5*cell_x), (margin+cell_y)*y+margin+(0.5*cell_y) ))
 					screen.blit(bomb, text_rect)
+					lost = True
 
 	 
 		# --- Go ahead and update the screen with what we've drawn.
